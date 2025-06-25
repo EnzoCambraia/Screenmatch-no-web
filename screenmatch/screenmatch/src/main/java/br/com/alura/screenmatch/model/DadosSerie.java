d6e1f04b546c64c43a646d7af0ce4f3dd33409f0
Enzo Cambraia <enzoc.unb@gmail.com>
2025-06-25 02:10:58 -0300

    Início do projeto Spring

---
+++ screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,14 @@
+package br.com.alura.screenmatch.model;
+// O fato de usarmos record aqui é por que não vamos mexer com esses dados, apenas representá-los posteriormente
+// @JsonAlias apelidará o campo de atributo do Json como nos casos abaixo oque antes era Title com o JsonAlias passa a se referir como titulo
+// @JsonProperty servirá tanto para serialização de Jsons como a deserialização também, fazendo o processo de ler e escrever a mesma coisa que leu no Json
+// sendo que o JsonAlias apenas lê de forma referenciada e não escreve.
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true) // Estamos dizendo para o Jackson não tentar converter oque não foi mapeado, por padrão, vem false, setamos para true para não ocorrer essa tentativa
+// que trigga um erro chamado UnrecognizedPropertyException
+
+public record DadosSerie(@JsonAlias("Title") String titulo, @JsonAlias("totalSeasons") Integer totalTemporadas, @JsonAlias("imdbRating") String avaliacao) {
+}
